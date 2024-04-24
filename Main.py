a96b36f948581c118200ae7b5784303d3cf32517
iArtistTree <89613806+iArtistTree@users.noreply.github.com>
2024-04-24 12:10:21 -0400

    Background added

--- Main.py
+++ Main.py
@@ -14,7 +14,6 @@ clock = pygame.time.Clock()
 FPS = 60
 
 #images
-
 plrSprite = pygame.image.load('./assets/images/CharSpreadSheet.png').convert_alpha()
 
 def get_image(sheet, frame, x, y, scale):
@@ -23,6 +22,12 @@ def get_image(sheet, frame, x, y, scale):
     image = pygame.transform.scale_by(image, (scale, scale))
 
     return image
+
+#create animation list
+animationlist = []
+animation_steps = 2
+lastUpdate = pygame.time.get_ticks()
+animationCooldown = 500
 
 #player class
 class Player():
@@ -34,6 +39,7 @@ class Player():
         self.posY = y
         self.speed = 10
         self.flip = False
+        self.status = 0
 
     def draw(self):
         screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
@@ -48,7 +54,6 @@ class Player():
 
         if keyPressed[pygame.K_w]:
             dy -= player.speed
-            self.image = get_image(plrSprite, 1 , 30, 30, 5)
 
         if keyPressed[pygame.K_a]:
             dx -= player.speed 
@@ -60,10 +65,20 @@ class Player():
         if keyPressed[pygame.K_d]:
             dx += player.speed 
             self.flip = False
+            self.walk()
 
         #update rectangle position
         self.rect.x += dx
         self.rect.y += dy
+
+    def idle(self):
+        self.image = get_image(plrSprite, 0 , 30, 30, 5)
+
+    def walk(self):
+        self.image = get_image(plrSprite, 1 , 30, 30, 5)
+
+        
+
 
 
 player = Player(0, 0)
